0e57326ded413b2fa774577633ca6c780b9ebdc8
matteo-didone <matteo.dido9@gmail.com>
2023-06-30 16:25:26 +0200

    Implemented basic scaffholind for the exercise

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Bad Words</title>
+    <!-- Font Awesome 6.4.0 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <!-- Form Section -->
+    <!-- Action is the receiver of my form's data -->
+    <!-- Method is the method that's being used to pass the data -->
+    <form action="" method="">
+        <!-- First Paragraph Field -->
+        <input type="text" name="">
+
+        <!-- Word that needs to be censored -->
+        <input type="text" name="">
+
+        <button type="submit">
+            Invia il form
+        </button>
+    </form>
+</body>
+
+</html>
